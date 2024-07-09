acf7dfcf062871f0694cb3472aa33a552d00cac7
STARRED787 <malithamalshan7@gmail.com>
2024-07-09 22:46:13 +0530

    c#

--- SchoolManagementSystem/Student.Designer.cs
+++ SchoolManagementSystem/Student.Designer.cs
@@ -35,8 +35,22 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tb_stid = new System.Windows.Forms.TextBox();
+            this.tb_stname = new System.Windows.Forms.TextBox();
+            this.tb_stdob = new System.Windows.Forms.TextBox();
+            this.tb_stgen = new System.Windows.Forms.TextBox();
+            this.tb_stnum = new System.Windows.Forms.TextBox();
+            this.tb_stemail = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -48,7 +62,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(759, 36);
+            this.panel1.Size = new System.Drawing.Size(989, 36);
             this.panel1.TabIndex = 1;
             // 
             // button1
@@ -57,7 +71,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.Maroon;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(695, 9);
+            this.button1.Location = new System.Drawing.Point(919, 9);
             this.button1.MaximumSize = new System.Drawing.Size(1920, 720);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(25, 23);
@@ -72,7 +86,7 @@
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
             this.button5.ForeColor = System.Drawing.Color.Maroon;
             this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
-            this.button5.Location = new System.Drawing.Point(726, 12);
+            this.button5.Location = new System.Drawing.Point(950, 12);
             this.button5.MaximumSize = new System.Drawing.Size(1920, 720);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(21, 20);
@@ -98,7 +112,7 @@
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(48)))), ((int)(((byte)(32)))));
             this.label1.Font = new System.Drawing.Font("Rockwell Extra Bold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(297, 15);
+            this.label1.Location = new System.Drawing.Point(409, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(158, 32);
             this.label1.TabIndex = 0;
@@ -111,14 +125,139 @@
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(0, 28);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(759, 83);
+            this.panel2.Size = new System.Drawing.Size(989, 83);
             this.panel2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tb_stemail);
+            this.groupBox1.Controls.Add(this.tb_stnum);
+            this.groupBox1.Controls.Add(this.tb_stgen);
+            this.groupBox1.Controls.Add(this.tb_stdob);
+            this.groupBox1.Controls.Add(this.tb_stname);
+            this.groupBox1.Controls.Add(this.tb_stid);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(965, 480);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student Information";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Student Id";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Student Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Date Of Birth";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(44, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Gender";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(44, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 23);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Phone Number";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(44, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 23);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Email";
+            // 
+            // tb_stid
+            // 
+            this.tb_stid.Location = new System.Drawing.Point(235, 43);
+            this.tb_stid.Name = "tb_stid";
+            this.tb_stid.Size = new System.Drawing.Size(208, 26);
+            this.tb_stid.TabIndex = 6;
+            // 
+            // tb_stname
+            // 
+            this.tb_stname.Location = new System.Drawing.Point(235, 82);
+            this.tb_stname.Name = "tb_stname";
+            this.tb_stname.Size = new System.Drawing.Size(208, 26);
+            this.tb_stname.TabIndex = 7;
+            // 
+            // tb_stdob
+            // 
+            this.tb_stdob.Location = new System.Drawing.Point(235, 129);
+            this.tb_stdob.Name = "tb_stdob";
+            this.tb_stdob.Size = new System.Drawing.Size(208, 26);
+            this.tb_stdob.TabIndex = 8;
+            // 
+            // tb_stgen
+            // 
+            this.tb_stgen.Location = new System.Drawing.Point(235, 166);
+            this.tb_stgen.Name = "tb_stgen";
+            this.tb_stgen.Size = new System.Drawing.Size(208, 26);
+            this.tb_stgen.TabIndex = 9;
+            // 
+            // tb_stnum
+            // 
+            this.tb_stnum.Location = new System.Drawing.Point(235, 205);
+            this.tb_stnum.Name = "tb_stnum";
+            this.tb_stnum.Size = new System.Drawing.Size(208, 26);
+            this.tb_stnum.TabIndex = 10;
+            // 
+            // tb_stemail
+            // 
+            this.tb_stemail.Location = new System.Drawing.Point(235, 247);
+            this.tb_stemail.Name = "tb_stemail";
+            this.tb_stemail.Size = new System.Drawing.Size(208, 26);
+            this.tb_stemail.TabIndex = 11;
             // 
             // Student
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(759, 626);
+            this.ClientSize = new System.Drawing.Size(989, 626);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -130,6 +269,8 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -141,5 +282,18 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_stemail;
+        private System.Windows.Forms.TextBox tb_stnum;
+        private System.Windows.Forms.TextBox tb_stgen;
+        private System.Windows.Forms.TextBox tb_stdob;
+        private System.Windows.Forms.TextBox tb_stname;
+        private System.Windows.Forms.TextBox tb_stid;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
     }
 }
