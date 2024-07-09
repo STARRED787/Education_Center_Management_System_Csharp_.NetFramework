225977719bec4d20821f72c4d28288a685371ac2
STARRED787 <malithamalshan7@gmail.com>
2024-07-10 02:05:02 +0530

    C#

---
+++ Database files/StudentInfoTb.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[StudentInfoTb] (
+    [Student_Id]   INT           NOT NULL,
+    [Student_Name] VARCHAR (100) NOT NULL,
+    [Dob]          DATETIME      NOT NULL,
+    [Gender]       VARCHAR (10)  NOT NULL,
+    [Phone_Num]    VARCHAR (10)  NOT NULL,
+    [Email]        VARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Student_Id] ASC)
+);
+
