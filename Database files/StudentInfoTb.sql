ef572cb0f8eb7d4cdba9f285707673e82935ed9a
STARRED787 <malithamalshan7@gmail.com>
2024-09-04 01:08:29 +0530

    C#

--- Database files/StudentInfoTb.sql
+++ Database files/StudentInfoTb.sql
@@ -1,12 +1,12 @@
-﻿CREATE TABLE [dbo].[StudentInfoTb] (
-    [Student_Id]   INT           NOT NULL,
-    [Student_Name] VARCHAR (100) NOT NULL,
-    [Dob]          DATETIME      NOT NULL,
-    [Gender]       VARCHAR (10)  NOT NULL,
-    [Phone_Num]    VARCHAR (10)  NOT NULL,
-    [Email]        VARCHAR (50)  NULL,
-    [Subject] VARCHAR(100) NOT NULL, 
-    [Subject_Section] VARCHAR(100) NOT NULL, 
+﻿CREATE TABLE [dbo].[StudentInfoTbl] (
+    [Student_Id]      INT           NOT NULL,
+    [Student_Name]    VARCHAR (100) NOT NULL,
+    [Dob]             DATETIME      NOT NULL,
+    [Gender]          VARCHAR (10)  NOT NULL,
+    [Phone_Num]       VARCHAR (10)  NOT NULL,
+    [Email]           VARCHAR (50)  NOT NULL,
+    [Subject]         VARCHAR (100) NOT NULL,
+    [Subject_Section] VARCHAR (100) NOT NULL,
     PRIMARY KEY CLUSTERED ([Student_Id] ASC)
 );
 
